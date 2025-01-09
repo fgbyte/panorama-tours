6d01aa71ceae19a58e26d0ab741d29331bd456b3
fgbyte <fer.garciabayon@gmail.com>
2025-01-09 10:44:31 -0500

    feat: add favicon link and new logo asset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+			</head>
 			<body
 				className={`min-h-screen bg-background font-sans antialiased max-w-6xl m-auto text-black
 					${inter.className}`}
