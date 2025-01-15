f995abd6556821216da96db3d46c18f75bbde241
fgbyte <fer.garciabayon@gmail.com>
2025-01-15 14:54:50 -0500

    add starting point

--- src/components/TourContent.tsx
+++ src/components/TourContent.tsx
@@ -1,8 +1,11 @@
 import mock from "@/data/mock.json";
+import { SquareArrowOutUpRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { WhatsAppIcon } from "./Icons";
-import { Button } from "./ui/button";
+import GoogleMapLocation from "./google-map-location";
+
+const mapApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
 const phone = "41796915893";
 
@@ -47,7 +50,7 @@ export default function TourContent({ id }: { id: string }) {
 			</div>
 
 			{/* Details Section */}
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8 sm:mb-12">
 				<div>
 					<h2 className="text-2xl font-bold mb-4">What's Included</h2>
 					<ul className="list-disc list-inside">
@@ -80,11 +83,10 @@ export default function TourContent({ id }: { id: string }) {
 				</div>
 			</div>
 
-			{/* Itinerary Section */}
-			{/* TODO: add start point info */}
-			<div className="mb-8">
-				<h2 className="text-2xl font-bold mb-4">Itinerary</h2>
-				<div className="space-y-4 grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+				{/* Itinerary Section */}
+				<div>
+					<h2 className="text-2xl font-bold mb-4">Itinerary</h2>
 					<div>
 						{tour.itinerary[0].day > 0
 							? tour.itinerary.map((day) => (
@@ -113,7 +115,20 @@ export default function TourContent({ id }: { id: string }) {
 									</ul>
 								))}
 					</div>
-					<div className="flex flex-col gap-2">
+				</div>
+				{/* Start Point Section */}
+				<div>
+					<h2 className="text-2xl font-bold mb-2">Start Point</h2>
+					<Link
+						href={(tour.location.startingPoint as { url: string }).url}
+						target="_blank"
+						className=" flex gap-1 items-center hover:text-green-800 cursor-pointer"
+					>
+						{`📌 ${(tour.location.startingPoint as { title: string }).title}`}
+						<SquareArrowOutUpRight className="w-4 h-4" />
+					</Link>
+					{/* Contact Button */}
+					<div className="flex flex-col gap-2 mt-8">
 						<p className="text-red-600 text-center sm:text-start font-semibold">
 							At least {tour.id === "tour_001" ? 2 : 3} days in advance
 						</p>
@@ -133,6 +148,7 @@ export default function TourContent({ id }: { id: string }) {
 				<div className="masonry sm:masonry-sm">
 					{tour.images.map((image, index) => (
 						<Image
+							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
 							key={index}
 							width={500}
 							height={500}
